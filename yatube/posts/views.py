7a3e1141cb7cdb777c3145efc011ce14cafcad24
Maga Khashiev <outgush27@gmail.com>
2023-02-09 23:26:12 +0300

    Добавлены urls и view-функция

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# Главная страница
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Перед тобой {slug}')
