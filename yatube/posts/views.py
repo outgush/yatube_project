362689389ebc2dbb6fc6f8d4533a55be5a0e09da
Maga Khashiev <outgush27@gmail.com>
2023-02-22 18:00:53 +0300

    Код после завершения теории

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 # Create your views here.
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'title': text,
+               'posts': posts}
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Перед тобой {slug}')
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
